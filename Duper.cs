63b8071206daf2fdfa3296113daede84432e2cb8
neilsimp1 <neilsimp1@gmail.com>
2015-12-22 12:26:16 -0500

    initial commit

---
+++ Duper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace duper{
+	class Duper{
+
+		private string cwd, filename;
+		private List<string> currentDups;
+
+		public Duper(string cwd, string filename){
+			this.cwd = cwd;
+			this.filename = filename;
+			this.currentDups = getCurrentDups();
+		}
+
+		public void dupe(){
+			bool has1 = currentDups.Contains(filename + ".001")
+				,has2 = currentDups.Contains(filename + ".002")
+				,has3 = currentDups.Contains(filename + ".003")
+				,has4 = currentDups.Contains(filename + ".004")
+				,has5 = currentDups.Contains(filename + ".005");
+
+			if(has5 && currentDups.Count == 5) File.Delete(cwd + "\\" + currentDups[4]);
+			if(has4) File.Copy(cwd + "\\" + filename + ".004", cwd + "\\" + filename + ".005", true);
+			if(has3) File.Copy(cwd + "\\" + filename + ".003", cwd + "\\" + filename + ".004", true);
+			if(has2) File.Copy(cwd + "\\" + filename + ".002", cwd + "\\" + filename + ".003", true);
+			if(has1) File.Copy(cwd + "\\" + filename + ".001", cwd + "\\" + filename + ".002", true);
+			File.Copy(cwd + "\\" + filename, cwd + "\\" + filename + ".001", true);
+		}
+
+		private List<string> getCurrentDups(){
+			List<string> currentDups = new List<string>();
+
+			foreach(string filepath in Directory.GetFiles(cwd).OrderBy(f => f)){
+				string _filename = Path.GetFileName(filepath);
+				if(_filename.Contains(filename) && _filename != filename) currentDups.Add(_filename);
+			}
+
+			return currentDups;
+		}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Windows.Forms;
+
+namespace duper{
+	class Program{
+		
+		public static void Main(string[] args){
+			string parentProcessName = getParentProcessName();
+			string outputMethod = parentProcessName == "cmd" || parentProcessName == "powershell" || parentProcessName == "devenv" ? "console" : "msgbox";
+
+			if(args.Length != 1){
+				if(outputMethod == "console"){
+					Console.WriteLine("Invalid arguments");
+					Console.Read();
+				}
+				else MessageBox.Show("Invalid arguments");
+				return;
+			}
+
+			string cwd = Path.GetDirectoryName(args[0]) != String.Empty ? Path.GetDirectoryName(args[0]) : Directory.GetCurrentDirectory();
+			string filename = Path.GetFileName(args[0]);
+
+			Duper duper = new Duper(cwd, filename);
+			duper.dupe();
+        }
+
+		private static string getParentProcessName(){
+			int myId = Process.GetCurrentProcess().Id;
+			string query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", myId);
+			ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", query);
+			ManagementObjectCollection.ManagementObjectEnumerator results = searcher.Get().GetEnumerator();
+			results.MoveNext();
+			ManagementObject queryObj = (ManagementObject)results.Current;
+			uint parentId = (uint)queryObj["ParentProcessId"];
+			Process parent = Process.GetProcessById((int)parentId);
+
+			return parent.ProcessName.ToLower();
+		}
+		
+	}
+}
